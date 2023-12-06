69e64f90a15b5e51c96d489671442cf5d436a14d
michaelimmanuel <michael.mimo24@gmail.com>
2023-12-06 09:30:44 +0700

    added login and register fragment and navigation

--- app/src/main/java/com/example/uas/RetrofitClient.kt
+++ app/src/main/java/com/example/uas/RetrofitClient.kt
@@ -4,7 +4,7 @@ import com.example.uas.api.ExpenseApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.119:3000"
+    private const val BASE_URL = "http://172.21.224.37:3000"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
